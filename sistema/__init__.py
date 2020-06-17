a3281dd7a17c43d53b6c7b5c554639080067267e
Marcelo Ceolin <mceolin@unidavi.edu.br>
2020-06-17 14:53:51 -0300

    refatorado init.py e adicionado registrado ping blueprint

--- sistema/__init__.py
+++ sistema/__init__.py
@@ -4,34 +4,23 @@ from flask import Flask, jsonify
 from flask_restx import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
-api = Api(app)
-
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object('sistema.config.DevelopmentConfig')
-
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-class Usuario(db.Model):
-    __tablename__ = 'usuarios'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    usuario = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    ativo = db.Column(db.Boolean, default=True, nullable=False)
+def create_app(script_info=None):
+    app = Flask(__name__)
 
-    def __init__(self, usuario, email):
-        self.usuario = usuario
-        self.email = email
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
+    db.init_app(app)
 
-class Ping(Resource):
-    def get(self):
-        return {
-            'status': 'sucesso',
-            'message': 'pong'
-        }
+    from sistema.api.ping import ping_blueprint
+    app.register_blueprint(ping_blueprint)
 
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
 
-api.add_resource(Ping, '/ping')
+    return app
