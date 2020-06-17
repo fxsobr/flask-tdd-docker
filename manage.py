e8982c1c0c477a50a8fabd4656dfadeefa2292ff
Marcelo Ceolin <mceolin@unidavi.edu.br>
2020-06-17 14:33:26 -0300

    criado novo comando cli no arquivo manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,16 @@
 from flask.cli import FlaskGroup
 
-from sistema import app
+from sistema import app, db
 
 cli = FlaskGroup(app)
+
+
+@cli.command('recriar_db')
+def recriar_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 if __name__ == '__main__':
     cli()
