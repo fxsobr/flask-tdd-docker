5df1b6a945f14a93b972e2085bce79e613721b21
Marcelo Ceolin <mceolin@unidavi.edu.br>
2020-06-17 14:44:01 -0300

    criado classes de testes no test_config.py

--- sistema/tests/conftest.py
+++ sistema/tests/conftest.py
@@ -5,7 +5,7 @@ from sistema import app, db
 
 @pytest.fixture(scope='module')
 def test_app():
-    app.config.from_object('project.config.TestingConfig')
+    app.config.from_object('sistema.config.TestingConfig')
     with app.app_context():
         yield app
 
