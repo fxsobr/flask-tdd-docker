692112908e59d1a91a513dcfe20c17610de51ddf
Marcelo Ceolin <mceolin@unidavi.edu.br>
2020-06-17 14:16:22 -0300

    adicionado sqlalchemy e criado model usuario

--- sistema/__init__.py
+++ sistema/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -10,6 +11,19 @@ api = Api(app)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object('sistema.config.DevelopmentConfig')
 
+db = SQLAlchemy(app)
+
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    usuario = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    ativo = db.Column(db.Boolean, default=True, nullable=False)
+
+    def __init__(self, usuario, email):
+        self.usuario = usuario
+        self.email = email
 
 class Ping(Resource):
     def get(self):
