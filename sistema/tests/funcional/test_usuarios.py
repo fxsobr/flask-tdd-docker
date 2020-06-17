d3f9379098b0fabe2cc90981460f533642694c22
Marcelo Ceolin <mceolin@unidavi.edu.br>
2020-06-17 15:22:40 -0300

    criado test_usuarios

---
+++ sistema/tests/funcional/test_usuarios.py
@@ -0,0 +1,16 @@
+import json
+
+
+def test_adicionar_usuario(test_app, teste_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/usuarios',
+        data=json.dumps({
+            'usuario': 'mceolin',
+            'email': 'mceolin@unidavi.edu.br'
+        }),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 201
+    assert 'mceolin@unidavi.edu.br foi adicionado com sucesso!' in data['message']
